eec490c269b8713312888b4b24581bd7c5bdb64f
hanaaden <hanaadamabdi@gmail.com>
2025-06-28 20:04:51 +0300

    uncommented

--- classes/scopes.py
+++ classes/scopes.py
@@ -1,24 +1,24 @@
-#local scope
-# def func():
-#     x=100
-#     print(x)
+# local scope
+def func():
+    x=100
+    print(x)
     
-# func()
+func()
 
-# #enclosing scope
-# def out_func():
-#     x=900
-#     def ins_func():
-#         print(x)
-#     ins_func()
-# out_func()
+#enclosing scope
+def out_func():
+    x=900
+    def ins_func():
+        print(x)
+    ins_func()
+out_func()
 
-# #global scope
-# y=90
-# def myfunc():
-#     print(y)
-# myfunc()
-# print(y)
+#global scope
+y=90
+def myfunc():
+    print(y)
+myfunc()
+print(y)
 
 def scope_test():
     def do_local():
