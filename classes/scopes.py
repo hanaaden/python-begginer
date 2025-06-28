e8220a249f77838206d708603b13751faaf86569
hanaaden <hanaadamabdi@gmail.com>
2025-06-28 20:02:00 +0300

    namescape vs scope

--- classes/scopes.py
+++ classes/scopes.py
@@ -1,21 +1,42 @@
 #local scope
-def func():
-    x=100
-    print(x)
+# def func():
+#     x=100
+#     print(x)
     
-func()
+# func()
 
-#enclosing scope
-def out_func():
-    x=900
-    def ins_func():
-        print(x)
-    ins_func()
-out_func()
+# #enclosing scope
+# def out_func():
+#     x=900
+#     def ins_func():
+#         print(x)
+#     ins_func()
+# out_func()
 
-#global scope
-y=90
-def myfunc():
-    print(y)
-myfunc()
-print(y)
+# #global scope
+# y=90
+# def myfunc():
+#     print(y)
+# myfunc()
+# print(y)
+
+def scope_test():
+    def do_local():
+        spam = "local spam"
+    def do_nonlocal():
+        nonlocal spam
+        spam = "non local spam"
+    def do_global():
+        global spam
+        spam = "this is global spam"
+    spam = "this is testing"
+    do_nonlocal()
+    print("after local assigmet: " , spam)
+    do_local()
+    print("after non local assigmet: " , spam)
+    do_global()
+    print("after global assigmet: " , spam)
+
+scope_test()
+print("in global scope:", spam)
+        
