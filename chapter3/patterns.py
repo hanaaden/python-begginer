9f2532af5c098975b0eae94aa2d5c045c866825f
hanaaden <hanaadamabdi@gmail.com>
2025-07-20 17:04:46 +0300

    pattens

---
+++ chapter3/patterns.py
@@ -0,0 +1,26 @@
+def pyramid(n):
+ for i in range(n):
+        for k in range(i):
+            print("*" , end="")
+        print()
+        
+pyramid(5)
+
+rows = 6
+for i in range(rows):
+    for j in range(i):
+        print ( i , end = '')
+    print(i)
+    
+row = 5
+for i in range(1 , rows +  1):
+    for j in range(1 , i + 1):
+        print(j , end= '')
+    print('')
+    
+rows = 5
+num = rows
+for i in range(rows , 0 , -1):
+    for j in range(0 ,i):
+        print(num , end= '')
+    print("7")
